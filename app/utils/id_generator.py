7f0e29dd5a46e0b9fc3ee0f7afdacdd46807121f
nyakiomaina <nyakiomaina@users.noreply.github.com>
2026-02-09 11:29:35 +0000

    Remove Python app and tests

--- app/utils/id_generator.py
+++
@@ -1,8 +0,0 @@
-import secrets
-import string
-
-ALPHABET = string.ascii_lowercase + string.digits
-
-
-def generate_session_id(length: int = 12) -> str:
-    return "".join(secrets.choice(ALPHABET) for _ in range(length))
